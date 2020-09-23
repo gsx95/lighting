3043204de94eeba3d9afb52cbc7c134d0b5953ee
gsx95 <g.schwarz95@me.com>
2020-09-23 18:19:16 +0200

    remove deploy scripts

--- leds/data.go
+++ leds/data.go
@@ -1,10 +1,5 @@
 package leds
 
-
-const (
-	FullColorType = "full_color"
-)
-
 type ColorData struct {
 	Colors []uint32 `json:"colors"`
 }
--- redeploy.sh
+++
@@ -1,3 +0,0 @@
-export GOPATH=/home/ubuntu/leds/app
-go build .
-sudo systemctl restart lightingapp
